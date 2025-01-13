4c83cebb83895491b1a108e5d3d37ca9097b281e
lm <liming163vip@163.com>
2025-01-13 15:33:31 +0800

    [update] model about community

--- models/community.go
+++ models/community.go
@@ -1,7 +1,5 @@
 package models
 
-import "time"
-
 // Community 社区列表model
 type Community struct {
 	CommunityId   int64  `json:"community_id" db:"community_id"`
@@ -10,10 +8,10 @@ type Community struct {
 
 // CommunityDetail 社区详情model
 type CommunityDetail struct {
-	CommunityId   int64     `json:"community_id" db:"community_id"`
-	CommunityName string    `json:"community_name" db:"community_name"`
-	Introduction  string    `json:"introduction,omitempty" db:"introduction"`
-	CreateTime    time.Time `json:"create_time" db:"create_time"`
+	CommunityId   int64  `json:"community_id" db:"community_id"`
+	CommunityName string `json:"community_name" db:"community_name"`
+	Introduction  string `json:"introduction,omitempty" db:"introduction"`
+	CreateTime    string `json:"create_time" db:"create_time"`
 }
 
 // ApiCommunityDetailRes 社区列表接口响应数据
