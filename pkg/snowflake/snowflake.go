b9f02a539d42d7a3a534eabe23ad57336117562a
lm <liming163vip@163.com>
2025-02-03 18:49:17 +0800

    [update] func about snowflake

--- pkg/snowflake/snowflake.go
+++ pkg/snowflake/snowflake.go
@@ -1,6 +1,7 @@
 package snowflake
 
 import (
+	"strconv"
 	"time"
 
 	sf "github.com/bwmarrin/snowflake"
@@ -16,7 +17,17 @@ func Init(startTime string, machineId int64) (err error) {
 		return
 	}
 
-	sf.Epoch = st.UnixNano() / 1000000
+	// 标准雪花算法的分配：
+	// 1位符号位 + 41位时间戳 + 10位机器ID + 12位序列号 = 64位
+	// sf.NodeBits = uint8(10)
+	// sf.StepBits = uint8(12)
+
+	// 修改雪花算法的位数分配:
+	// 1位符号位 + 41位时间戳 + 5位机器ID + 6位序列号 = 53位
+	// sf.NodeBits = uint8(5) // 机器ID位数
+	// sf.StepBits = uint8(6) // 序列号位数
+
+	sf.Epoch = st.UnixNano() / 1000000 // 时间起点
 	node, err = sf.NewNode(machineId)
 	return
 }
@@ -24,4 +35,9 @@ func Init(startTime string, machineId int64) (err error) {
 // GetID 返回生成的id值
 func GetID() int64 {
 	return node.Generate().Int64()
+}
+
+// GetIDStr 返回字符串格式的id值
+func GetIDStr() string {
+	return strconv.FormatInt(node.Generate().Int64(), 10)
 }
