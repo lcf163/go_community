3a68fe26c3ca75f57935cff5bbec5cd2aea5bcea
lm <liming163vip@163.com>
2025-02-07 12:19:25 +0800

    [update] func about redis key

--- dao/redis/keys.go
+++ dao/redis/keys.go
@@ -5,10 +5,10 @@ package redis
 // redis key 注意使用命名空间的方式，方便查询和拆分
 const (
 	KeyPrefix                 = "go_community:"
-	KeyPostTimeZSet           = "post:time"   // 帖子及发帖时间
-	KeyPostScoreZSet          = "post:score"  // 帖子及投票分数
-	KeyPostVotedZSetPrefix    = "post:voted:" // 记录用户及投票类型
-	KeyCommunityPostSetPrefix = "community:"  // 保存每个分区下帖子的id
+	KeyPostTimeZSet           = "post:time"      // 帖子及发帖时间
+	KeyPostScoreZSet          = "post:score"     // 帖子及投票分数
+	KeyPostVotedZSetPrefix    = "post:voted:"    // 记录用户及投票类型
+	KeyCommunityPostSetPrefix = "community:"     // 保存每个分区下帖子的id
 	KeyCommentTimeZSet        = "comment:time"   // 评论及发布时间
 	KeyCommentVotedZSetPrefix = "comment:voted:" // 记录用户为评论投票的数据
 )
